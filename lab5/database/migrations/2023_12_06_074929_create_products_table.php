e3eb9f3ac2045ae47dafad9d0d7ced30fc7ee152
trungpd0911 <trungphan9dxd@gmail.com>
2023-12-17 22:16:22 +0700

    fix bug double

--- lab5/database/migrations/2023_12_06_074929_create_products_table.php
+++ lab5/database/migrations/2023_12_06_074929_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('ProductName');
-            $table->doule('SalePrice');
+            $table->double('SalePrice');
             $table->string('CategoryName');
             $table->string('ImageLink');
             $table->string('ProductLink');
